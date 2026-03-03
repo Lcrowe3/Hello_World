6749b580b7877ecbc7b0fa77a30365011405fd9f
LCrowe42 <LNCrowe@outlook.com>
2026-03-02 19:35:18 -0500

    Chapter 4

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     println!("Hello, world!");
     println!("{}", average(2.0, 7.4, 4.6));
+    println!("{}", celsius_to_fahrenheit(32.0));
 }
 
 fn average(a: f32, b: f32, c: f32) -> f32 {
     return (a + b + c) / 3.0;
+}
+
+fn celsius_to_fahrenheit(temp: f64) -> f64 {
+    return temp * 1.8 + 32.0;
 }
